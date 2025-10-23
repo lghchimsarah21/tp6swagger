64013c61deb5cfac016b339e23afcc14ff3122ee
lghchimsarah21 <lghchim.sarah@gmail.com>
2025-10-23 10:29:47 +0100

    first commit

---
+++ src/main/java/com/example/student_management/entity/Student.java
@@ -0,0 +1,52 @@
+package com.example.student_management.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nom;
+    private String prenom;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateNaissance;
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+}
